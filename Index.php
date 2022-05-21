1457375ec0455159238b2772ed948eaafa4e7638
Pamela-Birondo <90774995+Pamela-Birondo@users.noreply.github.com>
2022-05-21 23:17:39 +0800

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html> 
+<html>
+     <head>
+          <title>My Home Page</title>
+         <link rel="stylesheet" type="text/css" href="stylesheet.css">
+         <link rel="preconnect" href="https://fonts.googleapis.com">
+         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+         <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
+         <link rel="preconnect" href="https://fonts.googleapis.com">
+         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+         <link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet">
+         <link rel="preconnect" href="https://fonts.googleapis.com">
+         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@506&display=swap" rel="stylesheet">
+     </head> 
+     <body>
+         <div id="wrapper"> 
+             <div id="nav">
+                 <?php echo "<h1> My Portfolio </h1>";
+                 ?>
+                <div id="menu">
+                    <ul>
+                        <li><a href="Index.php">Home Page</a></li>
+                        <li><a href="activities.html">Activities</a></li>
+                        <li><a href="profile.php">Profile</a></li>
+                    
+                    </ul>
+                 
+                 </div>
+             
+             </div>
+             <div id="intro">
+             <?php
+                echo "<h1 align=center> Hi! I'm Pamela </h1>";
+                echo "<h1 align=center> I am an IT student and this is my portfolio.</h1>";
+                echo "<h1 align=center> In this portfolio you can see all of the learning tasks that I have made in Web Authoring Course. </h1> "
+   ?>
+             
+             </div>
+             
+             <div id="pics">
+                 <div id="one">
+                 
+                 </div>
+             
+             </div>
+         
+             </div>
+         <div id="footer">
+              <div id="box">
+               <a href="https://www.facebook.com/pamelaaaa09/"> <img src="img/fb_icon.png" alt="fb_logo"> </a>
+                <a href="https://www.instagram.com/akoosipam/"><img src="img/ig_icon.png" alt="ig_logo"> </a>
+                <a href="https://twitter.com/Faaaaam27"> <img src="img/twt_icon.png" alt="twt_logo"> </a>
+             </div>
+         </div>
+   </body>
+</html>
---
+++ profile.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Profile</title>
+       <link rel="stylesheet" type="text/css" href="stylesheet.css">
+         <link rel="preconnect" href="https://fonts.googleapis.com">
+         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+         <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
+         <link rel="preconnect" href="https://fonts.googleapis.com">
+         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+         <link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet">
+         <link rel="preconnect" href="https://fonts.googleapis.com">
+         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@506&display=swap" rel="stylesheet">
+         <link rel="preconnect" href="https://fonts.googleapis.com">
+         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300&display=swap" rel="stylesheet">
+    </head>
+        <body>
+            <div id="wrapperb">
+                <div id="nav">
+                  <h1>My Portfolio</h1>
+                    
+                    <div id="menu">
+                          <ul>
+                        <li><a href="Index.php">Home Page</a></li>
+                        <li><a href="activities.html">Activities</a></li>
+                        <li><a href="profile.php">Profile</a></li>
+                    
+                    </ul>
+                    </div>
+                
+                </div>
+                  <div id="outpicbox">
+                    <div id="picbox">
+                
+                
+                </div>
+                      </div>
+                <div id="namebox">
+                      <h1>Pamela Denise A. Birondo</h1>
+                </div>
+                <div id="detailbox">
+                     <div id="leftbox">
+                         <h2>AGE:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;19 Years Old</h2>
+                         <br>
+                         <br>
+                         <br>
+                         <h2>DATE OF BIRTH:&nbsp;&nbsp;October 24, 2002</h2>
+                    </div>
+                 
+                    <div id="rightbox">
+                        <h2>ADDRESS: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;L.B. Jayme St. Talisay City </h2>
+                        <br>
+                        <h2>CONTACT NUMBER:&nbsp; 09569175944/09475825478</h2>
+                        <br>
+                        <h2>EMAIL:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; pamelabirondo@gmail.com</h2>
+                    </div>
+                </div>
+            </div>
+            <div id="footer">
+              <div id="box">
+                <a href="https://www.facebook.com/pamelaaaa09/"> <img src="img/fb_icon.png" alt="fb_logo"> </a>
+                <a href="https://www.instagram.com/akoosipam/"><img src="img/ig_icon.png" alt="ig_logo"> </a>
+                <a href="https://twitter.com/Faaaaam27"> <img src="img/twt_icon.png" alt="twt_logo"> </a>
+             </div>
+         </div>
+   </body>
+</html>
+    
